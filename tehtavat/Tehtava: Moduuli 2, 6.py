7cafe04766dff5995c44c0cf6e1967feb1f7cd1a
Henrytar <Henry.Tarto@metropolia.fi>
2025-08-30 18:33:45 +0300

    toinen tunti

---
+++ tehtavat/Tehtava: Moduuli 2, 6.py
@@ -0,0 +1,5 @@
+import random
+koodi1 = str(random.randint(0,9))+str(random.randint(0,9))+str(random.randint(0,9))
+print(f"kolmenumeroisen koodi, jonka kukin numeromerkki on väliltä 0..9: {koodi1}")
+koodi2 = str(random.randint(1,6))+str(random.randint(1,6))+str(random.randint(1,6))+str(random.randint(1,6))
+print(f"nelinumweoinen koodi, jonka kukin numeromerkki on väliltä 1..6: {koodi2}")
